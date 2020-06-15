186a96f21c90cd2fcfc70b480a101cc2818d95ed
Sri <hsrinivas38@gmail.com>
2020-06-15 01:34:50 -0400

    React Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+
+function App() {
+  return (
+    <div className="App">
+<Greet />  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Greet(){
+    return <h1> Hello Siinu</h1>
+}
+
+const Greet = () => <h1> ssSs</h1>
+
+export default Greet
